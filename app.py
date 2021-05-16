dcfed92d140c7a580c222c6f5e0bc01ddd79c0d7
Pratyush Tulsian <pratyushtulsian@gmail.com>
2021-05-16 18:58:48 -0400

    setup flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+
+@app.route("/snapshot")
+def snapshot():
+    return {
+        'code': 'hi'
+    }
