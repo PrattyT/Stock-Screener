4e3f22697786313e91af1edfbcdd765c45250bdf
Pratyush Tulsian <pratyushtulsian@gmail.com>
2021-05-16 22:52:04 -0400

    added charts from finviz

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, csv
 import numpy
 import talib
 from flask import Flask, render_template, request
@@ -11,20 +11,33 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     pattern = request.args.get('pattern', default = None)
+    stocks = {}
+
+    with open('datasets/companies.csv') as f:
+        for row in csv.reader(f) :
+            stocks[row[0]] = {'company' : row[1]}
+
+
     if pattern:
         datafiles = os.listdir('datasets/daily')
         for filename in datafiles:
             df = pd.read_csv('datasets/daily/{}'.format(filename))
             pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
             try:
                 result = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
                 last = result.tail(1).values[0]
-                if last != 0:
-                    print("{} triggered {}".format(filename, pattern))
+
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:  
+                    stocks[symbol][pattern] = None
             except:
                 pass
 
-    return render_template('index.html', patterns = patterns)
+    return render_template('index.html', patterns = patterns, stocks = stocks, current_pattern = pattern)
 
 
 @app.route("/snapshot")
